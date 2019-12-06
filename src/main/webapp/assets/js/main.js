dfed88f83b5ef8197a23943ea3f6baffe04bb59f
Евгений Маслов <evgescha.maslov007@yandex.ru>
2019-12-06 10:20:52 +0300

    fix изменения значений переменных на стороне клиента

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -39,26 +39,26 @@ function next() {
 
 // чтение переменных
 function addVariable(vrbl) {
-	console.log("Add variable "+vrbl["key"]+":"+vrll["value"]));
+	console.log("Add variable "+vrbl["key"]+":"+vrbl["value"]);
 	variables.set(vrbl["key"], vrbl["value"]);
 
 
 }
 // изменение переменной на заданное значение
-function updateNumericVariable(var key, var value){
-	console.log("Update variable "+key+", old:"variables[key]+", add:"+value);
+function updateNumericVariable( key,  value){
+	console.log("Update variable "+key+", old:"+variables[key]+", add:"+value);
 	var val = variables.get(key);
 		val = Number.parseInt(val); // Теперь будет числом
 		variables.set(key, val+value);
-		console.log("new value: "+variables.get(key);
+		console.log("new value: "+variables.get(key));
 // if (val.match(/^\d+$/)) {
 // }
 }
 // изменение не числовых переменных
-function updateNonNumericVariable(var key, var value){
-	console.log("Update variable "+key+", old:"variables[key]+", to:"+value);
+function updateNonNumericVariable( key,  value){
+	console.log("Update variable "+key+", old:"+variables[key]+", to:"+value);
 	variables.set(key, value);
-	console.log("new value: "+variables.get(key);
+	console.log("new value: "+variables.get(key));
 }
 
 
