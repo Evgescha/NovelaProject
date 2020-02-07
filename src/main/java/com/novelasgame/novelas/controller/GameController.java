83850e0daf466a3940ccce810f1102c5fd244017
Yauheni_Maslau <Yauheni_Maslau@EPBYVITW0076.minsk.epam.com>
2020-02-07 17:36:28 +0300

    try add delete game for one user

--- src/main/java/com/novelasgame/novelas/controller/GameController.java
+++ src/main/java/com/novelasgame/novelas/controller/GameController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.novelasgame.novelas.entity.Game;
-import com.novelasgame.novelas.entity.User;
 import com.novelasgame.novelas.service.GameService;
-import com.novelasgame.novelas.service.UserServiceImpl;
 
 @Controller
 @RequestMapping("/games")
--- src/main/java/com/novelasgame/novelas/service/GameService.java
+++ src/main/java/com/novelasgame/novelas/service/GameService.java
@@ -3,8 +3,6 @@ package com.novelasgame.novelas.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import com.novelasgame.novelas.entity.Game;
